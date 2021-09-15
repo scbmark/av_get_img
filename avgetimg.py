6cf2f1a61afe75ecd7112fd64e42e8937833ad74
scbmark <scbmark@gmail.com>
2021-09-15 19:19:41 +0800

    3.0

--- avgetimg.py
+++ avgetimg.py
@@ -1,10 +1,10 @@
 #!/home/scbmark/anaconda3/bin/python3.8
 #爬取av01的封面圖
 #作者：scbmark
-#日期：2021/9/12
-#版本：2.0(python)
+#日期：2021/9/15
+#版本：3.0(python)
 
-import bs4
+from bs4 import BeautifulSoup
 from requests import get,post
 import sys
 from pathlib import Path
@@ -39,37 +39,36 @@ headers={
 t=0
 while t==0:
     # 輸入番號
-    print("請輸入番號 例如:mide-365 ,mide365 或輸入'q'結束程式")
+    print("\n請輸入番號 例如:mide-365 ,mide365 或輸入'q'結束程式")
     num=input("番號：")
     if num=="q":
         sys.exit("程式結束")
     else:
-        data={"sn":f'{num}'}
-        # 取得html檔
-        root=post(url,headers=headers,data=data)
+            data={"sn":f'{num}'}
+            # 取得html檔
+            root=post(url,headers=headers,data=data)
 
-        # 解析html
-        htmlfile=bs4.BeautifulSoup(root.text,"lxml")
+            # 解析html
+            htmlfile=BeautifulSoup(root.text,"lxml")
 
-        # 印出標題、片商、女優、發行日
-        title=htmlfile.find("h3")
-        print('標題：'+title.getText().strip(title.small.string))
+            # 印出標題、片商、女優、發行日
+            title=htmlfile.find("h3")
+            print('標題：'+title.getText().strip(title.small.string))
+            movinfo=htmlfile.find("div",class_="col-md-9")
+            pro_actress=movinfo.find_all("a",limit=2)
+            print('片商：'+pro_actress[1].string)
+            print('女優：'+pro_actress[0].string)
 
-        movinfo=htmlfile.find("div",class_="col-md-9")
-        pro_actress=movinfo.find_all("a",limit=2)
-        print('片商：'+pro_actress[1].string)
-        print('女優：'+pro_actress[0].string)
-
-        # 找出封面圖的標籤位置和網址
-        print('\n圖片下載中...')
-        img=htmlfile.find("div", class_="col-xs-12 col-md-12").find("img")
-        imglink=img["src"]
-        
-
-        # 下載封面圖並存檔
-        img=get(imglink,headers=headers)
-        imgnam=dowwloadpath+num.upper()+".jpg"
-        with open(imgnam,"wb") as file:
-            file.write(img.content)
-            print('-----下載完成-----\n')
-        file.close()
+            # 找出封面圖的標籤位置和網址
+            print('\n圖片下載中...')
+            img=htmlfile.find("div", class_="col-xs-12 col-md-12").find("img")
+            imglink=img["src"]
+            
+            # 下載封面圖並存檔
+            img=get(imglink,headers=headers)
+            filename=title.small.string[0:title.small.string.index(' ')]
+            imgnam=dowwloadpath+filename.upper()+".jpg"
+            with open(imgnam,"wb") as file:
+                file.write(img.content)
+                print('-----下載完成-----\n')
+            file.close()
